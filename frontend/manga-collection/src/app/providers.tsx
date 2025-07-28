ab9ddbc3fda3e51f14e60ebf60d7ec5a00a531d5
Eduardo Miyake <dudsmiyake@gmail.com>
2025-07-28 16:37:28 -0300

    removing user context from main

--- frontend/manga-collection/src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { UserProvider } from "@/context/UserContext";
-import { ReactNode } from "react";
-
-export function Providers({ children }: { children: ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
-}
